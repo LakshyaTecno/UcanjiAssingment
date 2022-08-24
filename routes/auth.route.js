21fcd28f4c6ea4fabf7e17aaf420aa9ac3dc9250
LakshyaTecno <LakshyaTecno@gmail.com>
2022-08-25 02:09:15 +0530

    edited routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -6,12 +6,12 @@ const {
 
 module.exports = (app) => {
   app.post(
-    "/crm/api/v2/auth/signup",
+    "/ucanji/api/v1/auth/signup",
     [validateSignUpRequestBody],
     authController.signup
   );
   app.post(
-    "/crm/api/v2/auth/signin",
+    "/ucan/api/v1/auth/signin",
     [validateSignInRequestBody],
     authController.signin
   );
